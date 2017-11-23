611b5cf5b9c3b1985690bd09735b068dc628d7b2
Bolik <Bolik@163.com>
2017-11-23 17:42:40 +0800

    docker in docker

---
+++ UpdateBuild.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+ROOTPATH=/Volumes/Work/OpenSource/WaterBolik/docker-demo
+# ROOTPATH=/Users/Bolik/OpenSource/WaterBolik/docker-demo
+
+# base image pull -------------------------------------------------------------
+docker pull alpine:latest
+docker pull alpine:3.6
+
+docker pull centos:latest
+docker pull centos:7.4.1708
+docker pull centos:6.9
+
+docker pull debian:latest
+docker pull debian:9.2
+
+docker pull ubuntu:latest
+docker pull ubuntu:16.04
+
+docker pull docker
+docker pull docker:dind
+
+# # docker pull mysql
+# docker pull mysql/mysql-server
+# docker pull postgres:alpine
+# docker pull adminer
+
+# os base images 
+docker build -t alpine_base ${ROOTPATH}/os_base/alpine_base/
+docker build -t centos_base ${ROOTPATH}/os_base/centos_base/
+docker build -t centos6_base ${ROOTPATH}/os_base/centos6_base/
+docker build -t centos_systemd ${ROOTPATH}/os_base/centos_systemd/
+docker build -t debian_base ${ROOTPATH}/os_base/debian_base/
+docker build -t ubuntu_base ${ROOTPATH}/os_base/ubuntu_base/
+
+# 查看Docker的磁盘使用情况
+docker system df
+
+# 清理dangling镜像(参考What are Docker : images?)和容器，以及失效的数据卷和网络。
+docker system prune -f
+
+# 查看Docker的磁盘使用情况
+docker system df
+
+# 列出所有本地镜像
+docker images
