47e0c4345c29226d7eff192928da3d2bf0056312
WaterBolik <WaterBolik@163.com>
2017-11-26 23:11:40 +0800

    https://testdriven.io/part-one-docker-config/

--- flask/flask-microservices-users/app/project/__init__.py
+++ flask/flask-microservices-users/app/project/__init__.py
@@ -1,6 +1,6 @@
 # project/__init__.py
 
-
+import os
 from flask import Flask, jsonify
 
 
@@ -8,8 +8,10 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # set config
-app.config.from_object('project.config.DevelopmentConfig')
-
+# app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+# print(app.config)
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
