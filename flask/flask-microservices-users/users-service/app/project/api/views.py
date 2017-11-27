bd3400f98ab9fae1b819de7bff5806f996ed2973
WaterBolik <WaterBolik@163.com>
2017-11-27 23:41:15 +0800

    https://testdriven.io/part-one-restful-routes/ GET Single User

--- flask/flask-microservices-users/users-service/app/project/api/views.py
+++ flask/flask-microservices-users/users-service/app/project/api/views.py
@@ -1,8 +1,8 @@
 # project/api/views.py
-
-
-from flask import Blueprint, jsonify
-
+from flask import Blueprint, jsonify, request
+from project import db
+from project.api.models import User
+from sqlalchemy import exc
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -13,3 +13,90 @@ def ping_pong():
         'status': 'success',
         'message': 'pong!'
     })
+
+# @users_blueprint.route('/users', methods=['POST'])
+# def add_user():
+#     post_data = request.get_json()
+#     username = post_data.get('username')
+#     email = post_data.get('email')
+#     db.session.add(User(username=username, email=email))
+#     db.session.commit()
+#     response_object = {
+#         'status': 'success',
+#         'message': f'{email} was added!'
+#     }
+#     return jsonify(response_object), 201
+
+@users_blueprint.route('/users', methods=['POST'])
+def add_user():
+    post_data = request.get_json()
+    if not post_data:
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+    username = post_data.get('username')
+    email = post_data.get('email')
+    try:
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            db.session.add(User(username=username, email=email))
+            db.session.commit()
+            response_object = {
+                'status': 'success',
+                'message': f'{email} was added!'
+            }
+            return jsonify(response_object), 201
+        else:
+            response_object = {
+                'status': 'fail',
+                'message': 'Sorry. That email already exists.'
+            }
+            return jsonify(response_object), 400
+    except exc.IntegrityError as e:
+        db.session.rollback()
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        return jsonify(response_object), 400
+
+
+# @users_blueprint.route('/users/<user_id>', methods=['GET'])
+# def get_single_user(user_id):
+#     """Get single user details"""
+#     user = User.query.filter_by(id=user_id).first()
+#     response_object = {
+#         'status': 'success',
+#         'data': {
+#             'username': user.username,
+#             'email': user.email,
+#             'created_at': user.created_at
+#         }
+#     }
+#     return jsonify(response_object), 200
+
+@users_blueprint.route('/users/<user_id>', methods=['GET'])
+def get_single_user(user_id):
+    """Get single user details"""
+    response_object = {
+        'status': 'fail',
+        'message': 'User does not exist'
+    }
+    try:
+        user = User.query.filter_by(id=int(user_id)).first()
+        if not user:
+            return jsonify(response_object), 404
+        else:
+            response_object = {
+                'status': 'success',
+                'data': {
+                  'username': user.username,
+                  'email': user.email,
+                  'created_at': user.created_at
+                }
+            }
+            return jsonify(response_object), 200
+    except ValueError:
+        return jsonify(response_object), 404
--- flask/flask-microservices-users/users-service/app/project/tests/test_users.py
+++ flask/flask-microservices-users/users-service/app/project/tests/test_users.py
@@ -1,8 +1,8 @@
 # project/tests/test_users.py
-
-
 import json
 
+from project import db
+from project.api.models import User
 from project.tests.base import BaseTestCase
 
 
@@ -16,3 +16,102 @@ class TestUserService(BaseTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertIn('pong!', data['message'])
         self.assertIn('success', data['status'])
+
+    def test_add_user(self):
+        """Ensure a new user can be added to the database."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('michael@realpython.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_add_user_invalid_json(self):
+        """Ensure error is thrown if the JSON object is empty."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict()),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_invalid_json_keys(self):
+        """Ensure error is thrown if the JSON object does not have a username key."""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(email='michael@realpython.com')),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_add_user_duplicate_user(self):
+        """Ensure error is thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps(dict(
+                    username='michael',
+                    email='michael@realpython.com'
+                )),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn(
+                'Sorry. That email already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = User(username='michael', email='michael@realpython.com')
+        db.session.add(user)
+        db.session.commit()
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue('created_at' in data['data'])
+            self.assertIn('michael', data['data']['username'])
+            self.assertIn('michael@realpython.com', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+
+    def test_single_user_incorrect_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
