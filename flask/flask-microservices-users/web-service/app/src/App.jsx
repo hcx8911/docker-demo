7b413eecb4cceffcbbabe1efda895aef20038cb2
WaterBolik <WaterBolik@163.com>
2017-12-04 22:44:07 +0800

    https://testdriven.io/part-three-react-router/

--- flask/flask-microservices-users/web-service/app/src/App.jsx
+++ flask/flask-microservices-users/web-service/app/src/App.jsx
@@ -1,38 +1,48 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { Route, Switch } from 'react-router-dom'
 
-import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
+import UsersList from "./components/UsersList";
+import AddUser from "./components/AddUser";
+import About from "./components/About";
 
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       users: [],
-      username: '',
-      email: ''
-    }
+      username: "",
+      email: ""
+    };
   }
   componentDidMount() {
     this.getUsers();
   }
   getUsers() {
-    axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
-    .then((res) => { this.setState({ users: res.data.data.users }); })
-    .catch((err) => { console.log(err); })
+    axios
+      .get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
+      .then(res => {
+        this.setState({ users: res.data.data.users });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   addUser(event) {
     event.preventDefault();
     const data = {
       username: this.state.username,
       email: this.state.email
-    }
-    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
-    .then((res) => {
-      this.getUsers();
-      this.setState({ username: '', email: '' });
-    })
-    .catch((err) => { console.log(err); })
+    };
+    axios
+      .post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then(res => {
+        this.getUsers();
+        this.setState({ username: "", email: "" });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
   handleChange(event) {
     const obj = {};
@@ -44,22 +54,34 @@ class App extends Component {
       <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <br/>
-            <h1>All Users</h1>
-            <hr/><br/>
-            <AddUser
-              username={this.state.username}
-              email={this.state.email}
-              handleChange={this.handleChange.bind(this)}
-              addUser={this.addUser.bind(this)}
-            />
-            <br/>
-            <UsersList users={this.state.users}/>
+            <br />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <div>
+                    <h1>All Users</h1>
+                    <hr />
+                    <br />
+                    <AddUser
+                      username={this.state.username}
+                      email={this.state.email}
+                      handleChange={this.handleChange.bind(this)}
+                      addUser={this.addUser.bind(this)}
+                    />
+                    <br />
+                    <UsersList users={this.state.users} />
+                  </div>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
